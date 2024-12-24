b5ec308ad1bcb17ed3a6852067b2de8b62eeda07
sergxlove <sergxlove@mail.ru>
2024-12-24 20:15:16 +0600

    bag fix

--- OnlineBank.Web/Program.cs
+++ OnlineBank.Web/Program.cs
@@ -7,11 +7,7 @@ namespace OnlineBank.Web
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", async (context) =>
-            {
-                context.Response.ContentType = "text/html; charset=utf-8";
-                await context.Response.SendFileAsync(@"wwwroot\\index.html");
-            });
+            app.UseStaticFiles();
 
             app.Run();
         }
