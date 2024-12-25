ee45f1c2085ef171d759ee4a9ed47d666febda40
sergxlove <sergxlove@mail.ru>
2024-12-26 04:07:01 +0600

    add logics login

--- OnlineBank.Web/Program.cs
+++ OnlineBank.Web/Program.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices;
+
 namespace OnlineBank.Web
 {
     public class Program
@@ -9,8 +12,28 @@ namespace OnlineBank.Web
 
             app.UseStaticFiles();
 
-
-
+            app.Map("/login", () =>
+            {
+                return Results.File("pages/formLogIn.html", "text/html;");
+            });
+            app.MapPost("/api/users", async (HttpContext context) =>
+            {
+                app.Logger.LogInformation("done");
+                var reader = new StreamReader(context.Request.Body);
+                var json = await reader.ReadToEndAsync();
+                var dataContext = JObject.Parse(json);
+                if (dataContext is not null)
+                {
+                    string login = dataContext["login"]!.ToString();
+                    string password = dataContext["password"]!.ToString();
+                    if(login is not null &&  password is not null)
+                    {
+                        return Results.Ok();
+                    }
+                }
+                return Results.BadRequest();
+            });
+          
             app.Run();
         }
     }
---
+++ OnlineBank.Web/wwwroot/scripts/formLogIn.js
@@ -0,0 +1,35 @@
+﻿async function CheckUser(login, password) {
+    const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            login: login,
+            password: password
+        })
+    });
+    console.log("запрос отправлен");
+    if (response.ok == true) {
+        const header = document.querySelector("h3");
+        console.log("Данные успешно введены");
+        header.textContent = "Данные успешно введены";
+    }
+    else {
+        const header = document.querySelector("h3");
+        console.log("Произошла ошибка");
+        header.textContent = "Произошла ошибка";
+    }
+}
+document.getElementById("formLogin").addEventListener("submit", function (event) {
+    event.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("logInBtn").addEventListener("click", async () => {
+        const login = document.getElementById("first").value;
+        const password = document.getElementById("password").value;
+        await CheckUser(login, password);
+    });
+});
