2901bb7e12f4621a288f7add30ba171d01f204a9
sergxlove <sergxlove@mail.ru>
2025-01-03 00:21:46 +0600

    add database, update dbcontext, add two services

---
+++ OnlineBank.Application/Services/PasswordHasherService.cs
@@ -0,0 +1,30 @@
+﻿using OnlineBank.Application.Abstractions;
+using OnlineBank.Infrastructure.Abstractions;
+
+namespace OnlineBank.Application.Services
+{
+    public class PasswordHasherService : IPasswordHasherService
+    {
+        private readonly IPasswordHasher _passwordHasher;
+
+        public PasswordHasherService(IPasswordHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public string GenerateHashPassword(string password)
+        {
+            return _passwordHasher.Generate(password);
+        }
+
+        public bool ValidateSolve(string solve)
+        {
+            return _passwordHasher.Validate(solve);
+        }
+
+        public bool VerifyUser(string password, string hashPassword)
+        {
+            return _passwordHasher.Verify(password, hashPassword);
+        }
+    }
+}
