e0938ef67896ae19ddb79cf3fbb9c295c191b6b4
sergxlove <sergxlove@mail.ru>
2025-01-05 22:28:01 +0600

    add authentification and authorization, add jwt provider

--- OnlineBank.Web/Program.cs
+++ OnlineBank.Web/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Linq;
 using OnlineBank.Application.Abstractions;
 using OnlineBank.Application.Services;
@@ -6,6 +8,8 @@ using OnlineBank.DataAccess.Abstractions;
 using OnlineBank.DataAccess.Repositories;
 using OnlineBank.Infrastructure;
 using OnlineBank.Infrastructure.Abstractions;
+using System.Security.Claims;
+using System.Text;
 
 namespace OnlineBank.Web
 {
@@ -18,13 +22,58 @@ namespace OnlineBank.Web
             builder.Services.AddScoped<IUsersRepository, UsersRepository>();
             builder.Services.AddScoped<IUsersService, UsersService>();
             builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+            builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    IConfigurationSection? jwtSettings = builder.Configuration.GetSection("JwtSettings");
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtSettings["Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = jwtSettings["Audience"],
+                        ValidateLifetime = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!)),
+                        ValidateIssuerSigningKey = true
+                    };
+                    options.Events = new JwtBearerEvents()
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            context.Token = context.Request.Cookies["jwt"];
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("OnlyForAdmin", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "admin");
+                });
+                options.AddPolicy("OnlyForAuthUser", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "user");
+                });
+            });
+
             var app = builder.Build();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseStaticFiles();
 
             app.Map("/login", () =>
             {
                 return Results.File("pages/formLogIn.html", "text/html;");
+            });
+            app.Map("/logout", (HttpContext context) =>
+            {
+                context.Response.Cookies.Delete("jwt");
+                return Results.Ok();
             });
             app.Map("/registr", () =>
             {
