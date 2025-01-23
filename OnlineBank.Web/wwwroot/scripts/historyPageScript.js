f7134b10d0d00b2bba3773fa12b82f8293f03628
sergxlove <sergxlove@mail.ru>
2025-01-23 20:34:02 +0600

    update static files

--- OnlineBank.Web/wwwroot/scripts/historyPageScript.js
+++
@@ -1,9 +0,0 @@
-function updateDateTime() {
-    const now = new Date();
-    const options = { year: 'numeric', month: 'long', day: 'numeric', hour12: false };
-    const dateTimeString = now.toLocaleString('ru-RU', options);
-    
-    document.getElementById('datetime').innerText = dateTimeString;
-}
-
-updateDateTime();
