d57e5e4c9b8caecda16c5d3d269ad0f53b78ecc0
sergxlove <sergxlove@mail.ru>
2025-01-29 01:22:19 +0600

    add new table in database, add dbset, configurations and repository system table

---
+++ OnlineBank.DataAccess/Repositories/SystemTableRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBank.DataAccess.Abstractions;
+
+namespace OnlineBank.DataAccess.Repositories
+{
+    public class SystemTableRepository : ISystemTableRepository
+    {
+        private readonly DbContextSqlite _context;
+
+        public SystemTableRepository(DbContextSqlite context)
+        {
+            _context = context;
+        }
+
+        public async Task<long> Get()
+        {
+            int id = 777;
+            var table = await _context.SystemTable
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.Id == id);
+            return table!.NumberCard;
+        }
+
+        public async Task<int> Increment()
+        {
+            int id = 777;
+            long numberCard = await Get() + 1;
+            return await _context.SystemTable
+                .AsNoTracking()
+                .Where(a => a.Id == id)
+                .ExecuteUpdateAsync(s => s.SetProperty(a => a.NumberCard, numberCard));
+        }
+
+        public async Task<long> GetAndIncrement()
+        {
+            long numberCard = await Get();
+            await Increment();
+            return numberCard;
+        }
+    }
+}
+
