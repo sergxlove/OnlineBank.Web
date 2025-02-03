dea0031a3b21626b0e80140f488d6aa634dc639a
sergxlove <sergxlove@mail.ru>
2025-02-03 21:44:08 +0600

    add new methods in card service and change enpoint createCard

--- OnlineBank.Application/Services/CardsService.cs
+++ OnlineBank.Application/Services/CardsService.cs
@@ -8,10 +8,12 @@ namespace OnlineBank.Application.Services
     public class CardsService : ICardsService
     {
         private readonly ICardsRepository _repository;
+        private readonly ISystemTableRepository _systemRepository;
 
-        public CardsService(ICardsRepository repository)
+        public CardsService(ICardsRepository repository, ISystemTableRepository systemRepository)
         {
             _repository = repository;
+            _systemRepository = systemRepository;
         }
 
         public async Task<Guid> AddNewCard(string numberCard, string dateEnd, string cvv, Guid userId)
@@ -32,6 +34,24 @@ namespace OnlineBank.Application.Services
         public async Task<Guid?> VerifyCard(string numberCard, string dateEnd, string cvv)
         {
             return await _repository.Verify(numberCard, dateEnd, cvv);
+        }
+
+        public async Task<string> GenerateNumberCard()
+        {
+            long number = await _systemRepository.GetAndIncrement();
+            return "7700" + number.ToString();
+        }
+
+        public string GenerateDateEnd()
+        {
+            string date = DateOnly.FromDateTime(DateTime.UtcNow).ToString();
+            string result = string.Concat(date[3], date[4], "/", date[8], date[9]);
+            return result;
+        }
+
+        public string GenerateCvv()
+        {
+            return Convert.ToString(Random.Shared.Next(100, 999 + 1));
         }
     }
 }
